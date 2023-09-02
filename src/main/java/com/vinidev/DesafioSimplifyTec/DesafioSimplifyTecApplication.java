217bd390f1548c1a03fa9a44217575d09ae55727
vinicius-carv <valves8956@gmail.com>
2023-09-02 11:59:26 -0300

    Projeto spring iniciado

---
+++ src/main/java/com/vinidev/DesafioSimplifyTec/DesafioSimplifyTecApplication.java
@@ -0,0 +1,13 @@
+package com.vinidev.DesafioSimplifyTec;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DesafioSimplifyTecApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DesafioSimplifyTecApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/vinidev/DesafioSimplifyTec/DesafioSimplifyTecApplicationTests.java
@@ -0,0 +1,13 @@
+package com.vinidev.DesafioSimplifyTec;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DesafioSimplifyTecApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
