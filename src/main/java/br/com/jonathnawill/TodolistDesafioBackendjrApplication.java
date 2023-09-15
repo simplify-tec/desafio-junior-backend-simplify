ef4435897f78e48289e93575f0993d220d6478b8
Jonathnawill <Jjoninha11@hotmail.com>
2023-09-15 15:35:29 -0300

    Created Project

---
+++ src/main/java/br/com/jonathnawill/TodolistDesafioBackendjrApplication.java
@@ -0,0 +1,13 @@
+package br.com.jonathnawill;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TodolistDesafioBackendjrApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TodolistDesafioBackendjrApplication.class, args);
+	}
+
+}
---
+++ src/test/java/br/com/jonathnawill/TodolistDesafioBackendjrApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.jonathnawill;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TodolistDesafioBackendjrApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
